b12433bb19c9ded28e670bd01b017b6e03e48aa4
Jennifer Currie <renestl@gmail.com>
2020-09-19 14:30:52 -0500

    Create a database

--- banking/banking.py
+++ banking/banking.py
@@ -1,170 +1,157 @@
-# Write your code here
 import random
-
-all_accounts = {}
-
-
-def print_menu():
-    print("1. Create an account")
-    print("2. Log into account")
-    print("0. Exit")
+import _sqlite3
 
 
-def generate_checksum(acct_num):
-    nums = list(acct_num)
+def drop_table():
+    cur.execute('DROP TABLE IF EXISTS card')
 
-    for index in range(len(nums)):
+
+def create_table():
+    cur.execute('''CREATE TABLE card
+                    (id INTEGER, 
+                    number TEXT,
+                    pin TEXT,
+                    balance INTEGER DEFAULT 0)''')
+
+
+def insert_new_acct(number, pin, balance):
+    id_calc = cur.execute('SELECT count(*) FROM card')
+    acct_id = id_calc.fetchone()[0] + 1
+    cur.execute(f'INSERT INTO card VALUES ({acct_id}, {number}, {pin}, {balance})')
+    update_database()
+
+
+def fetch_acct(num, pin):
+    # cards = cur.execute('SELECT * FROM card')
+    cards = cur.execute('SELECT * FROM card WHERE (number=? and pin=?)', (num, pin,))
+    return cards.fetchone()
+
+
+def update_database():
+    # commit changes
+    conn.commit()
+
+
+def check_lyhn(card):
+    check_sum = 0  # digit 16-19 depending on card, verification digit
+
+    for index in range(len(card)):
         if index % 2 == 0:
-            nums[index] = int(nums[index]) * 2
+            n = int(card[index]) * 2
+            check_sum += n - 9 if n > 9 else n
         else:
-            nums[index] = int(nums[index])
+            check_sum += int(card[index])
 
-    for index in range(len(nums)):
-        if int(nums[index]) > 9:
-            nums[index] = int(nums[index]) - 9
-        else:
-            nums[index] = int(nums[index])
-
-    nums_sum = sum(nums)
-
-    if nums_sum % 10 == 0:
-        return 0
+    if check_sum % 10 == 0:
+        final_num = 0
     else:
-        check = nums_sum + (10 - nums_sum % 10)
-        return check - nums_sum
+        final_num = 10 - (check_sum % 10)
+
+    return final_num
 
 
-def create_account():
-    temp_account_num = []
+class BankingSystem:
+    def __init__(self):
+        self.card_num = 0
+        self.pin_num = 0
+        self.acct_balance = 0
+        self.main_menu()
 
-    mii = "4"  # digit 1
-    iin = "00000"  # digits 2 - 6
-    account_id = [str(random.randint(0, 9)) for _ in range(9)]  # digit 7 to n - 1, with n being between 15 and 18
+    def main_menu(self):
+        while True:
+            print("1. Create an account")
+            print("2. Log into account")
+            print("0. Exit")
 
-    temp_pin_num = [str(random.randint(0, 9)) for _ in range(4)]
-    balance = "0"
+            menu_choice = input()
 
-    temp_account_num.append(mii)
-    temp_account_num.append(iin)
-    temp_account_num.append("".join(account_id))
-
-    generate_account_num = "".join(temp_account_num)
-
-    checksum = generate_checksum(generate_account_num)  # digit 16-19 depending on card, verification digit
-    temp_account_num.append(str(checksum))
-
-    account_num = "".join(temp_account_num)
-    pin_num = "".join(temp_pin_num)
-
-    all_accounts[account_num] = {'pin': pin_num, 'balance': balance}
-
-    print('Your card has been created')
-    print('Your card number:')
-    print(account_num)
-    print('Your card PIN:')
-    print(pin_num)
-    print()
-
-
-def customer_menu(account):
-    print()
-    print("1. Balance")
-    print("2. Log out")
-    print("0. Exit")
-
-    customer_menu_choice = input()
-
-    if customer_menu_choice == "1":
-        print()
-        print(f"Balance: {account['balance']}")
-    elif customer_menu_choice == "2":
-        print()
-        print('You have successfully logged out!')
-        print_menu()
-    else:
-        print()
-        print('Bye')
-        exit()
-
-
-def validate_checksum(card_num):
-    nums = list(card_num)
-
-    print(nums)
-    for index in range(len(nums) - 1):
-        if index % 2 == 0:
-            nums[index] = int(nums[index]) * 2
-        else:
-            nums[index] = int(nums[index])
-
-    for index in range(len(nums) - 1):
-        if int(nums[index]) > 9:
-            nums[index] = int(nums[index]) - 9
-        else:
-            nums[index] = int(nums[index])
-
-    nums[-1] = int(nums[-1])
-    print(nums)
-    nums_sum = sum(nums)
-
-    if nums_sum % 10 == 0:
-        return True
-    else:
-        return False
-
-
-def verify_account(card, pin_num):
-    if card in all_accounts:
-        if pin_num == all_accounts[card]['pin']:
-            if validate_checksum(card):
+            if menu_choice == "1":
+                self.create_account()
+            elif menu_choice == "2":
+                # pass
+                self.acct_login()
+            elif menu_choice == "0":
                 print()
-                print('You have successfully logged in!')
-                customer = all_accounts[card]
-                customer_menu(customer)
+                print('Bye')
+                conn.close()
+                exit()
             else:
+                print("Invalid Menu Entry")
                 print()
-                print("Wrong card number or PIN!")
-                print()
-                print_menu()
-                log_in_account()
+
+    def create_account(self):
+        # generate card number
+        mii = "4"  # digit 1
+        iin = "00000"  # digits 2 - 6
+        acct_num = str(random.randint(100000000, 999999998))  # digit 7 to n - 1, with n being between 15 and 18
+        self.card_num = str(mii + iin + acct_num)
+
+        check_sum = check_lyhn(self.card_num)
+        self.card_num = self.card_num + str(check_sum)
+
+        # generate pin number
+        self.pin_num = str(random.randint(0000, 9999)).zfill(4)
+
+        self.acct_balance = 0
+
+        # store new account info
+        insert_new_acct(self.card_num, self.pin_num, self.acct_balance)
+
+        # print new account info
+        print('Card length')
+        print(len(self.card_num))
+        print()
+        print('Your card has been created')
+        print('Your card number:')
+        print(self.card_num)
+        print('Your card PIN:')
+        print(self.pin_num)
+        print()
+
+    def acct_login(self):
+        print()
+        card = input('Enter your card number: \n')
+        pin = input('Enter your PIN: \n')
+
+        if fetch_acct(card, pin) is not None:
+            print()
+            print('You have successfully logged in!')
+            print()
+            self.login_success()
         else:
             print()
             print("Wrong card number or PIN!")
-    else:
-        print()
-        print("Wrong card number or PIN!")
-
-
-def log_in_account():
-    card_num = input('Enter your card number: \n')
-    user_pin = input('Enter your PIN: \n')
-
-    verify_account(card_num, user_pin)
-    print()
-
-
-def program():
-    print_menu()
-    user_menu_choice = input()
-    print()
-
-    while True:
-        if user_menu_choice == "1":
-            create_account()
-            # print()
-        elif user_menu_choice == "2":
-            print()
-            log_in_account()
-        elif user_menu_choice == "0":
-            print()
-            print('Bye')
-            break
-        else:
-            print("Invalid Menu Entry")
             print()
 
-        print_menu()
-        user_menu_choice = input()
+    def login_success(self):
+        while True:
+            print("1. Balance")
+            print("2. Log out")
+            print("0. Exit")
+
+            customer_menu_choice = input()
+
+            if customer_menu_choice == "1":
+                print()
+                print(f"Balance: {self.acct_balance}")
+                print()
+            elif customer_menu_choice == "2":
+                print('You have successfully logged out!')
+                print()
+                self.main_menu()
+            elif customer_menu_choice == "0":
+                print()
+                print('Bye')
+                conn.close()
+                exit()
+            else:
+                print("Invalid Menu Entry")
+                print()
 
 
-program()
+if __name__ == '__main__':
+    conn = _sqlite3.connect('card.s3db')
+    cur = conn.cursor()
+    drop_table()
+    create_table()
+    bank = BankingSystem()
